eb54b230136467dcba851353303af3f3e3b5198a
Tharun <kumartharun435@gmail.com>
2023-09-01 00:02:03 +0530

    Added Merge Sorted Lists

---
+++ LinkedLists/MergeTwoSortedLists/MergeNodes.java
@@ -0,0 +1,24 @@
+package LinkedLists.MergeTwoSortedLists;
+
+public class MergeNodes {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode();
+        ListNode temp = dummy;
+
+        while(list1!= null && list2!=null){
+            if(list1.val<list2.val){
+                temp.next = list1;
+                list1 = list1.next;
+            }else{
+                temp.next = list2;
+                list2 = list2.next;
+            }
+            temp = temp.next;
+        }
+        if(list1!=null)
+            temp.next = list1;
+        else
+            temp.next = list2;
+        return dummy.next;
+    }
+}
--- LinkedLists/Readme.md
+++ LinkedLists/Readme.md
@@ -1,11 +1,14 @@
+<h2> Tips & Tricks </h2>
 
-<h3> Type of Problems / Algorithms to be Learnt </h3>
+* Always use Dummy head to point List elements if original head changes always.
+
+<h2> Type of Problems / Algorithms to be Learnt </h2>
 
  * Slow and Fast Pointer (Floyd's Hare and Tortoise)
- * 
+ 
 
 <h2> Interesting Problems </h2>
 
-
+ * Slow and Fast Pointer (Floyd's Hare and Tortoise)
 
 <h2> Best Tutorials and Blogs</h2>
