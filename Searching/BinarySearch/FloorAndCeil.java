c23b4545787ed8ecbda94e8e42526393a19ed43c
Tharun <kumartharun435@gmail.com>
2023-10-09 00:01:13 +0530

    Added Binary Search Basics

---
+++ Searching/BinarySearch/FloorAndCeil.java
@@ -0,0 +1,50 @@
+// package Searching.BinarySearch;
+public class FloorAndCeil {
+    static int findFloor(int[] arr, int n, int x) {
+        int low = 0, high = n - 1;
+        int ans = -1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            // maybe an answer
+            if (arr[mid] <= x) {
+                ans = arr[mid];
+                //look for smaller index on the left
+                low = mid + 1;
+            } else {
+                high = mid - 1; // look on the right
+            }
+        }
+        return ans;
+    }
+
+    // Finding Lower Bound in a Sorted Array
+    static int findCeil(int[] arr, int n, int x) {
+        int low = 0, high = n - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            // maybe an answer
+            if (arr[mid] >= x) {
+                ans = arr[mid];
+                //look for smaller index on the left
+                high = mid - 1;
+            } else {
+                low = mid + 1; // look on the right
+            }
+        }
+        return ans;
+    }
+    public static int[] getFloorAndCeil(int[] arr, int n, int x) {
+        int f = findFloor(arr, n, x);
+        int c = findCeil(arr, n, x);
+        return new int[] {f, c};
+    }
+    public static void main(String[] args) {
+        int[] arr = {3, 4, 4, 7, 8, 10};
+        int n = 6, x = 5;
+        int[] ans = getFloorAndCeil(arr, n, x);
+        System.out.println("The floor and ceil are: " + ans[0]
+                           + " " + ans[1]);
+    }
+}
---
+++ Searching/BinarySearch/Readme.md
@@ -0,0 +1,54 @@
+# Binary Search 
+
+* Time Complexity : O(logN)
+* mid = low + (high-low)/2
+
+```
+while(left <= right) {
+    int mid =  left + (right-left)/2 ;
+    if(nums[mid] < target) {
+        left = mid+1;
+    } else if(nums[mid] > target) {
+        right = mid-1;
+    }else {
+        return mid;
+    }
+}
+        
+```
+
+## Lower Bound
+The lower bound algorithm finds the first or the smallest index in a sorted array where the value at that index is greater than or equal to a given key i.e. x.
+
+```
+arr[ind] > = x; If no Index Found Lower Bound = Size of the Array
+
+while(low<=high){
+    int mid = low + (high-low)/2;
+    if(arr[mid]>=x){
+        res = mid;
+        lower_bound = mid-1;
+    }else{
+        low = mid+1;
+    }
+}
+return lower_bound;
+```
+
+## Upper Bound
+The lower bound algorithm finds the first or the smallest index in a sorted array where the value at that index is greater than or equal to a given key i.e. x.
+
+```
+arr[ind] > x; If no Index Found Lower Bound = Size of the Array
+
+while(low<=high){
+    int mid = low + (high-low)/2;
+    if(arr[mid]>x){
+        upper_bound = mid;
+        high = mid-1;
+    }else{
+        low = mid+1;
+    }
+}
+return upper_bound;
+```
