31003566473ca7445ab0e71c9fa148696a875702
Tharun <kumartharun435@gmail.com>
2023-10-28 22:23:49 +0530

    Added post Order Iterative Approach

--- Trees/BinaryTree/Basics/PostOrderTraversal.java
+++ Trees/BinaryTree/Basics/PostOrderTraversal.java
@@ -1,7 +1,6 @@
 package Trees.BinaryTree.Basics;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class PostOrderTraversal {
 
@@ -21,16 +20,38 @@ public class PostOrderTraversal {
         return postOrder;
 
     }
-    public static void main(String[] args) {
-    int[] arr = { 1, 2, 3, 4, 5, 6, 6, 6, 6 };
-    // Node root = insertLevelOrderRecursive(arr, 0);
-    Node root = CreateBinaryTree.insertLevelOrder(arr);
-    List<Integer> ans = postOrderTraversalRecursive(root);
-    System.out.println("Post Order Traversal");
-    // List<Integer> ans = preOrderTraversalRecursive(root);
-    for (int i = 0; i < ans.size(); i++) {
-      System.out.print(ans.get(i) + " ");
+
+    public static List<Integer> postorderTraversal(Node root) {
+        Stack<Node> s1 = new Stack<>();
+        Stack<Node> s2 = new Stack<>();
+        s1.push(root);
+        if (root == null)
+            return postOrder;
+        while (!s1.isEmpty()) {
+            Node curr = s1.pop();
+            s2.push(curr);
+            if (curr.left != null)
+                s1.push(curr.left);
+            if (curr.right != null)
+                s1.push(curr.right);
+        }
+        while (!s2.isEmpty()) {
+            postOrder.add(s2.pop().data);
+        }
+        return postOrder;
     }
 
-  }
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5, 6, 6, 6, 6 };
+        // Node root = insertLevelOrderRecursive(arr, 0);
+        Node root = CreateBinaryTree.insertLevelOrder(arr);
+        // List<Integer> ans = postOrderTraversalRecursive(root);
+        List<Integer> ans = postorderTraversal(root);
+        System.out.println("Post Order Traversal");
+        // List<Integer> ans = preOrderTraversalRecursive(root);
+        for (int i = 0; i < ans.size(); i++) {
+            System.out.print(ans.get(i) + " ");
+        }
+
+    }
 }
