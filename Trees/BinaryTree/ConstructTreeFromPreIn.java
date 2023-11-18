9c31b23117bb21323ecd66836534207e65e7bb61
Tharun <kumartharun435@gmail.com>
2023-11-18 20:15:32 +0530

    Construct Tree from Pre,Inorder

---
+++ Trees/BinaryTree/ConstructTreeFromPreIn.java
@@ -0,0 +1,27 @@
+package Trees.BinaryTree;
+
+import java.util.HashMap;
+
+public class ConstructTreeFromPreIn {
+    private int preorderIdx = 0;
+
+    public Node buildTree(int[] preorder, int[] inorder) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < inorder.length; i++)
+            map.put(inorder[i], i);
+        return construct(preorder, 0, inorder.length - 1, map);
+    }
+
+    public Node construct(int[] preorder, int left, int right, HashMap<Integer, Integer> map) {
+        if (left > right)
+            return null;
+        int pval = preorder[preorderIdx];
+        int inorderIdx = map.get(pval);
+
+        Node root = new Node(pval);
+        preorderIdx++;
+        root.left = construct(preorder, left, inorderIdx - 1, map);
+        root.right = construct(preorder, inorderIdx + 1, right, map);
+        return root;
+    }
+}
