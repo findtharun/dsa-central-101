b8692213ec6a14b93fed92431dff53c848d21575
Tharun <kumartharun435@gmail.com>
2023-12-25 11:41:35 +0530

    Detect Cycle in Directed Graph

---
+++ Graphs/DetectCycle/DirectedGraph/KahnBFS.java
@@ -0,0 +1,40 @@
+package Graphs.DetectCycle.DirectedGraph;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class KahnBFS {
+    public boolean isCyclic(int V, ArrayList<ArrayList<Integer>> adj) {
+        // code here
+        int[] inDegree = new int[V];
+        for (int i = 0; i < V; i++) {
+            for (int neighbour : adj.get(i)) {
+                inDegree[neighbour]++;
+            }
+        }
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < V; i++) {
+            if (inDegree[i] == 0)
+                q.offer(i);
+        }
+        // Cycle Found as There are no Nodes with InDegree 0
+        List<Integer> topologicalOrder = new ArrayList<>();
+        if (q.size() == 0)
+            return true;
+        while (q.size() > 0) {
+            int curr_node = q.peek();
+            topologicalOrder.add(curr_node);
+            q.poll();
+            for (int outgoingNode : adj.get(curr_node)) {
+                inDegree[outgoingNode]--;
+                if (inDegree[outgoingNode] == 0)
+                    q.offer(outgoingNode);
+            }
+        }
+        if (topologicalOrder.size() != V)
+            return true;
+        return false;
+    }
+}
