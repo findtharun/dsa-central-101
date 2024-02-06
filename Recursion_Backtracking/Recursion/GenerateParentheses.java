f8d22417e9a5aae9ed273a53850d9977d0bcd3b5
Tharun <kumartharun435@gmail.com>
2024-02-06 17:05:43 +0530

    Generate Parentheses

---
+++ Recursion_Backtracking/Recursion/GenerateParentheses.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenerateParentheses {
+    public static List<String> generateParenthesisAns(String p, int open, int close, int n, ArrayList<String> list) {
+        if (open == n && close == n) {
+            list.add(p);
+            return list;
+        }
+
+        if (open < n) {
+            generateParenthesisAns(p + "(", open + 1, close, n, list);
+        }
+        if (close < open) {
+            generateParenthesisAns(p + ")", open, close + 1, n, list);
+        }
+
+        return list;
+    }
+
+    public List<String> generateParenthesis(int n) {
+        return generateParenthesisAns("", 0, 0, n, new ArrayList<>());
+    }
+}
