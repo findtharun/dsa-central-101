b97a4ad0a8fe606bcf6a2a18cf8ce551ba1f19f2
Tharun <kumartharun435@gmail.com>
2024-03-18 23:27:01 +0530

    Ninja Training 2D DP

---
+++ DynamicProgramming/DP2D/NinjaTraining/Memoization.java
@@ -0,0 +1,33 @@
+package DynamicProgramming.DP2D.NinjaTraining;
+
+import java.util.Arrays;
+
+public class Memoization {
+    public static int helper(int day, int lastCompletedTask, int[][] points, int[][] dp) {
+        if (day == 0) {
+            int maxi = 0;
+            for (int task = 0; task < 3; task++) {
+                if (task != lastCompletedTask)
+                    maxi = Math.max(maxi, points[0][task]);
+            }
+            return dp[day][lastCompletedTask] = maxi;
+        }
+        if (dp[day][lastCompletedTask] != -1)
+            return dp[day][lastCompletedTask];
+        int maxi = 0;
+        for (int task = 0; task < 3; task++) {
+            if (task != lastCompletedTask) {
+                int currPoints = points[day][task] + helper(day - 1, task, points, dp);
+                maxi = Math.max(currPoints, maxi);
+            }
+        }
+        return dp[day][lastCompletedTask] = maxi;
+    }
+
+    public static int ninjaTraining(int n, int points[][]) {
+        int[][] dp = new int[n][4];
+        for (int[] row : dp)
+            Arrays.fill(row, -1);
+        return helper(n - 1, 3, points, dp);
+    }
+}
---
+++ DynamicProgramming/DP2D/NinjaTraining/Tabulation.java
@@ -0,0 +1,35 @@
+package DynamicProgramming.DP2D.NinjaTraining;
+
+public class Tabulation {
+    public static int ninjaTraining(int n, int[][] points) {
+        // Initialize a 2D array 'dp' to store the maximum points
+        int[][] dp = new int[n][4];
+
+        // Initialize the first day's maximum points based on the available choices
+        dp[0][0] = Math.max(points[0][1], points[0][2]); // 0th Task Performed
+        dp[0][1] = Math.max(points[0][0], points[0][2]); // 1st Task Performed
+        dp[0][2] = Math.max(points[0][0], points[0][1]); // 2nd Task Performed
+        dp[0][3] = Math.max(points[0][0], Math.max(points[0][1], points[0][2])); // If there is no activity Performed on
+                                                                                 // that day
+
+        // Iterate through each day and each activity
+        for (int day = 1; day < n; day++) {
+            for (int last = 0; last < 4; last++) {
+                dp[day][last] = 0; // Initialize the maximum points for the current day and last activity
+                // Consider each possible task for the current day
+                for (int task = 0; task <= 2; task++) {
+                    if (task != last) { // Ensure that the current task is different from the last
+                        // Calculate the points for the current activity and add it to the maximum
+                        // points from the previous day
+                        int activity = points[day][task] + dp[day - 1][task];
+                        // Update the maximum points for the current day and last activity
+                        dp[day][last] = Math.max(dp[day][last], activity);
+                    }
+                }
+            }
+        }
+
+        // Return the maximum points achievable after all days (last activity is 3)
+        return dp[n - 1][3];
+    }
+}
