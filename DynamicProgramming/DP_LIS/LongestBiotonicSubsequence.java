9ff26998fa086702c449f7275499a5ac16ff26d8
Tharun <kumartharun435@gmail.com>
2024-04-12 11:20:50 +0530

    Longest Biotonic Subsequence

---
+++ DynamicProgramming/DP_LIS/LongestBiotonicSubsequence.java
@@ -0,0 +1,34 @@
+package DynamicProgramming.DP_LIS;
+
+import java.util.*;
+// https://www.naukri.com/code360/problems/longest-bitonic-sequence_1062688
+public class LongestBiotonicSubsequence {
+    public static int longestBitonicSequence(int[] arr, int n) {
+        int[] dpLis = new int[n]; // For Longest Increasing Subsequence
+        int[] dpLds = new int[n]; // For Longest Decreasing Subsequence
+        Arrays.fill(dpLis, 1);
+        Arrays.fill(dpLds, 1);
+
+        for (int currInd = 1; currInd < n; currInd++) {
+            for (int prevInd = 0; prevInd < currInd; prevInd++) {
+                if ((arr[currInd] > arr[prevInd]) && (1 + dpLis[prevInd] > dpLis[currInd]))
+                    dpLis[currInd] = Math.max(dpLis[currInd], 1 + dpLis[prevInd]);
+            }
+        }
+
+        for (int currInd = n - 2; currInd >= 0; currInd--) {
+            for (int nextInd = n - 1; nextInd > currInd; nextInd--) {
+                if (arr[currInd] > arr[nextInd] && dpLds[currInd] < 1 + dpLds[nextInd])
+                    dpLds[currInd] = 1 + dpLds[nextInd];
+            }
+        }
+
+        int maxi = -1;
+
+        // Calculate the length of the longest bitonic subsequence
+        for (int i = 0; i < n; i++) {
+            maxi = Math.max(maxi, dpLis[i] + dpLds[i] - 1);
+        }
+        return maxi;
+    }
+}
