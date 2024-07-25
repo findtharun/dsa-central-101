5910933371224398bc71ad2fbe11d79a4f204be3
Tharun <kumartharun435@gmail.com>
2024-07-25 11:51:23 +0530

    Update Snake N Ladder

--- Graphs/BFS_DFS/SnakeNLadder/DefaultStyle.java
+++ Graphs/BFS_DFS/SnakeNLadder/DefaultStyle.java
@@ -18,32 +18,30 @@ class Pair {
 public class DefaultStyle {
     public static int minThrow(int N, int arr[]) {
         int[] moveVal = new int[31];
+        int destination = 30;
         Arrays.fill(moveVal, -1);
         for (int i = 0; i < 2 * N; i += 2)
             moveVal[arr[i]] = arr[i + 1]; // storing the moves in moves array
         // System.out.println(Arrays.toString(moves));
-        if (moveVal[30] != -1 && moveVal[30] < 30)
+        if (moveVal[destination] != -1 && moveVal[destination] < destination)
             return -1;
         boolean[] visited = new boolean[31];
         Queue<Pair> q = new LinkedList<>(); // creating a queue of pairs
         q.add(new Pair(1, 0));
+        visited[1] = true;
         while (!q.isEmpty()) {
             Pair curr = q.poll();
             int v = curr.v;
             int moves = curr.moves;
-            if (v == 30)
-                return moves;
-            for (int j = v + 1; j <= v + 6 && j <= 30; j++) {
-                if (visited[j] == false) {
-                    visited[j] = true;
-                    if (moveVal[j] != -1) {
-                        // If Snake or Ladder is found
-                        q.add(new Pair(moveVal[j], moves + 1));
-
-                    } else {
-                        // If no Snake or Ladder
-                        q.add(new Pair(j, moves + 1));
-                    }
+            for (int j = v + 1; j <= v + 6 && j <= destination; j++) {
+                int nextV = j;
+                if (moveVal[nextV] != -1) // Snake Or Ladder
+                    nextV = moveVal[nextV];
+                if (nextV == destination)
+                    return moves + 1;
+                if (!visited[nextV]) {
+                    q.add(new Pair(nextV, moves + 1));
+                    visited[nextV] = true;
                 }
             }
         }
