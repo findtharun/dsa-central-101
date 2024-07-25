37ca3429a6564afbabc85e429fa10d0b302bdcb6
Tharun <kumartharun435@gmail.com>
2024-07-25 12:10:45 +0530

    Boustrophedon style

---
+++ Graphs/BFS_DFS/SnakeNLadder/BoustrophedonStyle.java
@@ -0,0 +1,55 @@
+package Graphs.BFS_DFS.SnakeNLadder;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Pair {
+    int v;
+    int moves;
+
+    Pair(int v, int moves) {
+        this.v = v;
+        this.moves = moves;
+    }
+}
+
+public class BoustrophedonStyle {
+    public int[] getPos(int square, int n) {
+        int row = (square - 1) / n;
+        int col = (square - 1) % n;
+        // Since We have alternating Sequence, If row is Odd we have to Transform column
+        if (row % 2 == 1) {
+            col = n - 1 - col;
+        }
+        // We always need to Transform Row because of Boustrophedon style
+        row = n - 1 - row;
+        return new int[] { row, col };
+    }
+
+    public int snakesAndLadders(int[][] board) {
+        int n = board.length;
+        int destination = n * n;
+        boolean[] visited = new boolean[destination + 1];
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(1, 0));
+        visited[1] = true;
+        while (!q.isEmpty()) {
+            Pair curr = q.poll();
+            int v = curr.v;
+            int moves = curr.moves;
+            for (int j = v + 1; j <= v + 6 && j <= destination; j++) {
+                int[] pos = getPos(j, n); // Gives row and column of the
+                int nextV = j;
+                if (board[pos[0]][pos[1]] != -1) // Snake or Ladder
+                    nextV = board[pos[0]][pos[1]];
+                if (nextV == destination)
+                    return moves + 1;
+                if (visited[nextV] == false) {
+                    q.add(new Pair(nextV, moves + 1));
+                    visited[nextV] = true;
+                }
+            }
+        }
+        return -1;
+    }
+}
---
+++ Graphs/BFS_DFS/SnakeNLadder/Readme.md
@@ -0,0 +1,4 @@
+Time Complexity for any Style is O(N*N)
+
+In Boustrophedon style :  You are given an n x n integer matrix board where the cells are labeled from 1 to n2 and 1 is board[n-1][0] instead of [0][0].
+So we perform additional Computation to get the exact position of the given value in the board.
